17a67402bd0ffa891db836609bc26e37630e0d21
bill.zr <bill.zr@alibaba-inc.com>
2019-03-01 14:49:23 +0800

    添加日志打开快捷方式

---
+++ bash/console.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ALILANG_LOG_PATH=$(ls -t ~/Library/Logs/AliLangClient/ | tr '\t' '\n' | head -n 1)
+ALILANG_LOG_PATH="${HOME}/Library/Logs/AliLangClient/"$ALILANG_LOG_PATH
+
+HEMA_LOG_PATH=$(ls -t ~/Library/Logs/HemaClient/ | tr '\t' '\n' | head -n 1)
+HEMA_LOG_PATH="${HOME}/Library/Logs/HemaClient/"$HEMA_LOG_PATH
+
+
+function console {
+    case $1 in
+    hema) open -a /Applications/Utilities/Console.app "$HEMA_LOG_PATH" ;;
+    alilang) open -a /Applications/Utilities/Console.app "$ALILANG_LOG_PATH" ;;
+    esac
+}
+
+console $1

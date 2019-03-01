3a6d3956ad43c8eee3ac01db366906c8816d24cb
bill.zr <bill.zr@alibaba-inc.com>
2019-03-01 16:14:34 +0800

    修正podhook

--- bash/podhook.sh
+++ bash/podhook.sh
@@ -2,18 +2,19 @@
 
 /usr/bin/env pod $@
 
-function postInstallAction {
+function postInitAction {
     touch Podfile.new
     echo "
 ali_source 'alibaba-specs' #集团内部仓库
 ali_source 'alibaba-specs-mirror' #官方镜像仓库
-# include_official_source! #强制使用官方源
+#include_official_source! #强制使用官方源
 " >> Podfile.new
+# include_official_source! #强制使用官方源
     cat Podfile >> Podfile.new
     rm -f Podfile
     mv Podfile.new Podfile
 }
 
 case $1 in
-install) postInstallAction;;
+init) postInitAction;;
 esac

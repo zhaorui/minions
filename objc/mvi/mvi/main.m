1f413d2c8af68633218f0e0184a3c71b2ef2887f
bill.zr <bill.zr@alibaba-inc.com>
2019-03-31 16:53:35 +0800

    improve mpi

--- objc/mvi/mvi/main.m
+++ objc/mvi/mvi/main.m
@@ -12,6 +12,72 @@
 
 #include <getopt.h>
 
+#if defined(__HP_cc) && (__HP_cc >= 61000)
+#define NORETURN __attribute__((noreturn))
+#define NORETURN_PTR
+#elif defined(__GNUC__) && !defined(NO_NORETURN)
+#define NORETURN __attribute__((__noreturn__))
+#define NORETURN_PTR __attribute__((__noreturn__))
+#elif defined(_MSC_VER)
+#define NORETURN __declspec(noreturn)
+#define NORETURN_PTR
+#else
+#define NORETURN
+#define NORETURN_PTR
+#ifndef __GNUC__
+#ifndef __attribute__
+#define __attribute__(x)
+#endif
+#endif
+#endif
+
+static void vreportf(const char *prefix, const char *err, va_list params)
+{
+    char msg[4096];
+    char *p;
+    
+    vsnprintf(msg, sizeof(msg), err, params);
+    for (p = msg; *p; p++) {
+        if (iscntrl(*p) && *p != '\t' && *p != '\n')
+            *p = '?';
+    }
+    fprintf(stderr, "%s%s\n", prefix, msg);
+}
+
+static NORETURN void usage_builtin(const char *err, va_list params)
+{
+    vreportf("usage: ", err, params);
+    
+    /*
+     * Currently, the (err, params) are usually just the static usage
+     * string which isn't very useful here.  Usually, the call site
+     * manually calls fprintf(stderr,...) with the actual detailed
+     * syntax error before calling usage().
+     *
+     * TODO It would be nice to update the call sites to pass both
+     * the static usage string and the detailed error message.
+     */
+    
+    exit(129);
+}
+
+static void NORETURN usagef(const char *err, ...)
+{
+    va_list params;
+    
+    va_start(params, err);
+    usage_builtin(err, params);
+    va_end(params);
+}
+
+void NORETURN usage(const char *err)
+{
+    usagef("%s", err);
+}
+
+const char mvi_usage_string[] =
+"mvi <-f|--from> <-t|--to> <dir|file>";
+
 static void mvURL(NSURL* entryURL) {
     NSMutableArray* parentEntryComponents = [[entryURL pathComponents] mutableCopy];
     [parentEntryComponents removeLastObject];
@@ -54,13 +120,20 @@ int main(int argc, const char * argv[]) {
             }
         }
         
+        if (errorFlag) {
+            usage(mvi_usage_string);
+        }
+        
+        if (![config.patternRegex length] || ![config.templateRegex length]) {
+            usage(mvi_usage_string);
+        }
         
         if (optind < argc) {
             NSFileManager* fileMgr = [NSFileManager defaultManager];
             BOOL isD = NO;
             BOOL isE = NO;
             NSString *path = [NSString stringWithFileSystemRepresentation:argv[optind]];
-            NSURL* pathURL = [NSURL URLWithString:path];
+            NSURL* pathURL = [NSURL URLWithString:[path stringByStandardizingPath]];
             isE = [fileMgr fileExistsAtPath:path isDirectory:&isD];
             if (isE) {
                 if (isD) {
@@ -79,6 +152,7 @@ int main(int argc, const char * argv[]) {
                 }
             } else {
                 fprintf(stderr, "%s is not exist.\n", [path UTF8String]);
+                usage(mvi_usage_string);
             }
         }
     }
